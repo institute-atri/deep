022bd1602fc864a4144c694507a5ab929ad6fdea
Ygor Simões <contato.ygorsimoes@gmail.com>
2024-03-29 03:52:49 -0300

    feat: add base project structure with dependabot and Docker configurations
    This commit adds the initial project structure for a spring-boot application. It also includes the configuration for Docker and dependabot. Additionally, it includes a gitignore file to exclude IDE, OS-specific and build files. This setup helps to kickstart the development process.

---
+++ src/main/java/org/instituteatri/deep/infrastructure/entity/InvestigationEntity.java
@@ -0,0 +1,1 @@
+package org.instituteatri.deep.infrastructure.entity;
---
+++ src/test/java/org/instituteatri/deep/DeepApplicationTests.java
@@ -0,0 +1,13 @@
+package org.instituteatri.deep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
