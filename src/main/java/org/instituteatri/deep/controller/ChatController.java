361bad7c6300a4bd5ac563bebfc6dcc8eac78ae7
nicholasboari <nicholasboari@gmail.com>
2024-05-02 11:58:56 -0300

    fix: request to AI with history

--- src/main/java/org/instituteatri/deep/controller/ChatController.java
+++ src/main/java/org/instituteatri/deep/controller/ChatController.java
@@ -1,35 +1,34 @@
 package org.instituteatri.deep.controller;
 
-import java.util.Map;
+import java.util.List;
 
-import org.springframework.ai.chat.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaApi.Message;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/chat")
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final OllamaChatClient chatClient;
+    @Value("${spring.ai.ollama.chat.options.model}")
+    private String model;
 
     @GetMapping("/ai/generate/test")
-    public Map<String, String> generate(
-            @RequestParam(value = "message", defaultValue = "Alô, quem ta ai?") String message) {
-        return Map.of("generation", chatClient.call(message));
-    }
-
-    @GetMapping("/ai/generateStream/test")
-    public Flux<ChatResponse> generateStream(
-            @RequestParam(value = "message", defaultValue = "Alô, quem ta ai?") String message) {
-        Prompt prompt = new Prompt(message);
-        return chatClient.stream(prompt);
+    public OllamaApi.ChatResponse generate(
+            @RequestParam(value = "message") String message) {
+        Message msg1 = Message.builder(Message.Role.USER).withContent("Ola meu nome é Nicholas").build();
+        Message msg2 = Message.builder(Message.Role.ASSISTANT)
+                .withContent("Olá Nicholas! Prazer em conhecê-lo! Como posso ajudá-lo hoje?").build();
+        Message msg3 = Message.builder(Message.Role.USER).withContent(message).build();
+        OllamaApi.ChatRequest build = OllamaApi.ChatRequest.builder(model).withMessages(List.of(msg1, msg2, msg3))
+                .build();
+        return new OllamaApi().chat(build);
     }
 }
