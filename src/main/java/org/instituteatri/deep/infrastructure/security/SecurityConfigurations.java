7c7e0d72327d496264d455554894ef627bb481fb
Rafael Silva <ainbot6@gmail.com>
2024-05-03 21:56:36 -0300

    feat: add security configurations to application (#11)

---
+++ src/main/java/org/instituteatri/deep/infrastructure/security/SecurityConfigurations.java
@@ -0,0 +1,67 @@
+package org.instituteatri.deep.infrastructure.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfigurations {
+
+    final SecurityFilter securityFilter;
+    final AuthSecurityConfig authSecurityConfig;
+    final UserSecurityConfig userSecurityConfig;
+    final CustomLogoutHandler logoutHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        applyAuthSecurityConfig(httpSecurity);
+        applyUserSecurityConfig(httpSecurity);
+
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout -> logout.logoutUrl("/v1/auth/logout")
+                        .logoutSuccessUrl("/v1/auth/login")
+                        .addLogoutHandler(logoutHandler)
+                        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext()))
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    private void applyAuthSecurityConfig(HttpSecurity http) throws Exception {
+        authSecurityConfig.configure(http);
+    }
+
+    private void applyUserSecurityConfig(HttpSecurity http) throws Exception {
+        userSecurityConfig.configure(http);
+    }
+}
+
