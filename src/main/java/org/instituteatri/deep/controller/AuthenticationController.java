fb8fef2df339690a12dc933cf408ec77d089fb03
Rafael Silva <ainbot6@gmail.com>
2024-05-03 22:18:48 -0300

    update(authenticationcontroller): update controller for user authentication management (#11)

--- src/main/java/org/instituteatri/deep/controller/AuthenticationController.java
+++ src/main/java/org/instituteatri/deep/controller/AuthenticationController.java
@@ -1,18 +1,49 @@
 package org.instituteatri.deep.controller;
 
-import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.instituteatri.deep.dtos.user.AuthenticationDTO;
+import org.instituteatri.deep.dtos.user.RefreshTokenDTO;
+import org.instituteatri.deep.dtos.user.RegisterDTO;
+import org.instituteatri.deep.dtos.user.ResponseDTO;
+import org.instituteatri.deep.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final AuthenticationManager authenticationManager;
+    private final AccountService accountService;
+
     @PostMapping("/login")
-    public String getLogin() {
-        return "Hello, Deep!";
+    public ResponseEntity<ResponseDTO> getLogin(@RequestBody AuthenticationDTO authDto) {
+        return accountService.loginAccount(authDto, authenticationManager);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseDTO> register(@RequestBody @Valid RegisterDTO registerDTO) {
+        return accountService.registerAccount(registerDTO);
     }
 
     @PostMapping("/logout")
-    public String getLogout() {
-        return "Hello, Deep!";
+    public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        new SecurityContextLogoutHandler().logout(request, response, authentication);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<ResponseDTO> refreshToken(@RequestBody RefreshTokenDTO tokenDTO) {
+        return accountService.refreshToken(tokenDTO);
     }
 }
