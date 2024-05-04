d2d1d01923fcae5ebc342866dfd462a011b8d60c
Rafael Silva <ainbot6@gmail.com>
2024-05-03 21:09:52 -0300

    update(user): add properties to the user (#11)

--- src/main/java/org/instituteatri/deep/domain/user/User.java
+++ src/main/java/org/instituteatri/deep/domain/user/User.java
@@ -1,4 +1,96 @@
 package org.instituteatri.deep.domain.user;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.instituteatri.deep.domain.token.Token;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private boolean isEnable;
+
+    private int failedLoginAttempts = 0;
+
+    private LocalDateTime lockExpirationTime;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Token> tokens = new ArrayList<>();
+
+    public User(String name, String email, String password, boolean isEnable, UserRole role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.isEnable = isEnable;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return lockExpirationTime == null || lockExpirationTime.isBefore(LocalDateTime.now());
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnable;
+    }
+
+    public void lockAccountForHours() {
+        lockExpirationTime = LocalDateTime.now().plusHours(2);
+        isEnable = false;
+    }
 }
