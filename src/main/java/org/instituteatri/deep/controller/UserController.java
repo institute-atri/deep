9734c14f532c690a8b297e1935d1be7de1a202bf
Rafael Silva <ainbot6@gmail.com>
2024-05-03 22:19:49 -0300

    update(usercontroller): update controller for user management (#11)

--- src/main/java/org/instituteatri/deep/controller/UserController.java
+++ src/main/java/org/instituteatri/deep/controller/UserController.java
@@ -1,22 +1,45 @@
 package org.instituteatri.deep.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.instituteatri.deep.dtos.user.RegisterDTO;
+import org.instituteatri.deep.dtos.user.UserDTO;
+import org.instituteatri.deep.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
 
     @GetMapping()
-    public String getUsers() {
-        return "Hello, Deep!";
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<UserDTO> getByUserId(@PathVariable String id) {
+        UserDTO user = userService.getByUserId(id);
+        return ResponseEntity.ok().body(user);
     }
 
     @PutMapping("/update/{id}")
-    public String getUpdateById() {
-        return "Hello, Deep!";
+    public ResponseEntity<Void> updateUser(
+            @PathVariable String id,
+            @RequestBody @Valid RegisterDTO registerDTO,
+            Authentication authentication
+    ) {
+        return userService.updateUser(id, registerDTO, authentication);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+        return userService.deleteUser(id);
     }
 }
