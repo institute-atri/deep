7bf4cf29a277dca37f822a47f8359bdf07b90feb
Rafael Silva <ainbot6@gmail.com>
2024-05-03 21:11:31 -0300

    update(token): add properties to the token class (#11)

--- src/main/java/org/instituteatri/deep/domain/token/Token.java
+++ src/main/java/org/instituteatri/deep/domain/token/Token.java
@@ -1,4 +1,35 @@
 package org.instituteatri.deep.domain.token;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.instituteatri.deep.domain.user.User;
+
+
+@Data
+@Builder
+@Entity
+@Table(name = "tb_tokens")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    public String id;
+
+    @Column(length = 527)
+    public String tokenValue;
+
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+
+    public boolean revoked;
+
+    public boolean expired;
+
+    @ManyToOne
+    public User user;
 }
