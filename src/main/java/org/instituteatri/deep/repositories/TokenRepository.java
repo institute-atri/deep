daf590cd383476cafaaa6be04ebeecc39368df86
Rafael Silva <ainbot6@gmail.com>
2024-05-05 01:34:38 -0300

    feat: add method to delete tokens per user (#11)

--- src/main/java/org/instituteatri/deep/repositories/TokenRepository.java
+++ src/main/java/org/instituteatri/deep/repositories/TokenRepository.java
@@ -1,6 +1,7 @@
 package org.instituteatri.deep.repositories;
 
 import org.instituteatri.deep.domain.token.Token;
+import org.instituteatri.deep.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,8 +12,12 @@ import java.util.Optional;
 @Repository
 public interface TokenRepository extends JpaRepository<Token, String> {
 
-  @Query("SELECT t FROM Token t JOIN t.user u WHERE u.id = :id AND (t.tokenExpired = false OR t.tokenRevoked = false)")
-  List<Token> findAllValidTokenByUser(String id);
-  List<Token> findAllByUserId(String userId);
-  Optional<Token> findByTokenValue(String token);
+    @Query("SELECT t FROM Token t JOIN t.user u WHERE u.id = :id AND (t.tokenExpired = false OR t.tokenRevoked = false)")
+    List<Token> findAllValidTokenByUser(String id);
+
+    List<Token> findAllByUserId(String userId);
+
+    Optional<Token> findByTokenValue(String token);
+
+    void deleteByUser(User user);
 }
