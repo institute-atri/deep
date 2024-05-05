826758656ce254b15d3eeca90137cbdc568616aa
Rafael Silva <ainbot6@gmail.com>
2024-05-05 01:30:24 -0300

    update(usercontroller): update the ResponseEntity of the updateUser endpoint to return a ResponseDTO (#11)

--- src/main/java/org/instituteatri/deep/controller/UserController.java
+++ src/main/java/org/instituteatri/deep/controller/UserController.java
@@ -3,6 +3,7 @@ package org.instituteatri.deep.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.instituteatri.deep.dtos.user.RegisterDTO;
+import org.instituteatri.deep.dtos.user.ResponseDTO;
 import org.instituteatri.deep.dtos.user.UserDTO;
 import org.instituteatri.deep.service.UserService;
 import org.springframework.http.ResponseEntity;
@@ -30,7 +31,7 @@ public class UserController {
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<Void> updateUser(
+    public ResponseEntity<ResponseDTO> updateUser(
             @PathVariable String id,
             @RequestBody @Valid RegisterDTO registerDTO,
             Authentication authentication
