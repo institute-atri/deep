254a4faddafd4364488cbdb78372316d3980d6fe
Rafael Silva <ainbot6@gmail.com>
2024-05-06 01:55:35 -0300

    fix: small change to remove the public modifier (#11)

--- src/test/java/org/instituteatri/deep/controller/UserControllerTest.java
+++ src/test/java/org/instituteatri/deep/controller/UserControllerTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-public class UserControllerTest {
+class UserControllerTest {
 
     @Mock
     private UserService userService;
