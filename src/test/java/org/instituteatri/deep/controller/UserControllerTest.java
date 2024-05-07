7b59a98470b05b67d6ae564e9cb2591d61b0d9ab
Rafael Silva <ainbot6@gmail.com>
2024-05-07 02:46:30 -0300

    test: update user controller test (#11)

--- src/test/java/org/instituteatri/deep/controller/UserControllerTest.java
+++ src/test/java/org/instituteatri/deep/controller/UserControllerTest.java
@@ -4,23 +4,25 @@ import org.instituteatri.deep.dto.request.RegisterRequestDTO;
 import org.instituteatri.deep.dto.response.TokenResponseDTO;
 import org.instituteatri.deep.dto.response.UserResponseDTO;
 import org.instituteatri.deep.service.UserService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class UserControllerTest {
 
     @Mock
@@ -39,6 +41,7 @@ class UserControllerTest {
     private final Authentication authentication = Mockito.mock(Authentication.class);
 
     @Test
+    @DisplayName("When the service returns multiple users, it should return a list of users")
     void getAllUsers_WhenServiceReturnsMultipleUsers_ReturnsListOfUsers() {
         // Given
         List<UserResponseDTO> expectedUsers = Arrays.asList(
@@ -49,15 +52,17 @@ class UserControllerTest {
         when(userService.getAllUsers()).thenReturn(expectedResponse);
 
         // When
-        ResponseEntity<List<UserResponseDTO>> result = userController.getAllUsers();
+        ResponseEntity<List<UserResponseDTO>> responseEntity = userController.getAllUsers();
 
         // Then
-        assertEquals(HttpStatus.OK, result.getStatusCode());
-        assertEquals(expectedResponse, result);
-        assertEquals(expectedUsers, result.getBody());
+        assertThat(responseEntity)
+                .isEqualTo(expectedResponse)
+                .extracting(ResponseEntity::getStatusCode, ResponseEntity::getBody)
+                .containsExactly(HttpStatus.OK, expectedUsers);
     }
 
     @Test
+    @DisplayName("When the service returns an empty list, it must return an empty list")
     void testGetAllUsers_WhenServiceReturnsEmptyList_ShouldReturnEmptyList() {
         // Given
         List<UserResponseDTO> expectedUsers = Collections.emptyList();
@@ -65,32 +70,37 @@ class UserControllerTest {
         when(userService.getAllUsers()).thenReturn(expectedResponse);
 
         // When
-        ResponseEntity<List<UserResponseDTO>> result = userController.getAllUsers();
+        ResponseEntity<List<UserResponseDTO>> responseEntity = userController.getAllUsers();
 
         // Then
-        assertEquals(HttpStatus.OK, result.getStatusCode());
-        assertEquals(expectedResponse, result);
-        assertEquals(expectedUsers, result.getBody());
+        assertThat(responseEntity)
+                .isEqualTo(expectedResponse)
+                .extracting(ResponseEntity::getStatusCode, ResponseEntity::getBody)
+                .containsExactly(HttpStatus.OK, expectedUsers);
     }
 
 
     @Test
+    @DisplayName("When the ID is valid, it should return the corresponding user")
     void testGetUserById_WhenValidId_ThenReturnUser() {
         // Given
         String userId = "testId1";
-        UserResponseDTO expectedUser = new UserResponseDTO(userId, "testName1", "testEmail1@example.com");
-        when(userService.getByUserId(userId)).thenReturn(expectedUser);
+        UserResponseDTO expectedResponse = new UserResponseDTO(userId, "testName1", "testEmail1@example.com");
+        when(userService.getByUserId(userId)).thenReturn(expectedResponse);
 
         // When
-        ResponseEntity<UserResponseDTO> response = userController.getByUserId(userId);
+        ResponseEntity<UserResponseDTO> responseEntity = userController.getByUserId(userId);
 
         // Then
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(expectedUser, response.getBody());
+        assertThat(responseEntity.getBody())
+                .isEqualTo(expectedResponse);
+        assertThat(responseEntity.getStatusCode())
+                .isEqualTo(HttpStatus.OK);
     }
 
 
     @Test
+    @DisplayName("Upon receiving a valid request, it should return the updated user")
     void updateUser_WhenValidRequest_ThenReturnUpdatedUser() {
         // Given
         ResponseEntity<TokenResponseDTO> expectedResponse = ResponseEntity.ok().body(new TokenResponseDTO("Token", "RefreshToken"));
@@ -100,11 +110,14 @@ class UserControllerTest {
         ResponseEntity<TokenResponseDTO> responseEntity = userController.updateUser(id, registerRequestDTO, authentication);
 
         // Then
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(expectedResponse, responseEntity);
+        assertThat(responseEntity)
+                .isEqualTo(expectedResponse)
+                .extracting(ResponseEntity::getStatusCode)
+                .isEqualTo(HttpStatus.OK);
     }
 
     @Test
+    @DisplayName("Upon receiving a valid request, it should return an empty content response")
     void deleteUser_WhenValidRequest_ThenReturnNoContentResponse() {
         // Given
         ResponseEntity<Void> expectedResponse = ResponseEntity.noContent().build();
@@ -114,7 +127,9 @@ class UserControllerTest {
         ResponseEntity<Void> responseEntity = userController.deleteUser(id);
 
         // Then
-        assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());
-        assertEquals(expectedResponse, responseEntity);
+        assertThat(responseEntity)
+                .isEqualTo(expectedResponse)
+                .extracting(ResponseEntity::getStatusCode)
+                .isEqualTo(HttpStatus.NO_CONTENT);
     }
 }
