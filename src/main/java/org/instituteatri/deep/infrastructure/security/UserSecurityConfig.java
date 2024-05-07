c4a31d4ce556d9ca3b184c975063657a7f1e2074
Rafael Silva <ainbot6@gmail.com>
2024-05-06 21:28:24 -0300

    update(UserSecurityConfig): security configuration to allow access to specific endpoints for user management by the ADMIN and USER roles, keeping confidential operations restricted to administrators (#11)

--- src/main/java/org/instituteatri/deep/infrastructure/security/UserSecurityConfig.java
+++ src/main/java/org/instituteatri/deep/infrastructure/security/UserSecurityConfig.java
@@ -10,16 +10,17 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 @Configuration
 class UserSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
-    private static final String ADMIN_ROLE = "ADMIN";
+    private static final String ADMIN = "ADMIN";
+    private static final String USER = "USER";
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(HttpMethod.GET, "/v1/users").hasRole(ADMIN_ROLE)
-                .requestMatchers(HttpMethod.GET, "/v1/users/find/{id}").hasRole(ADMIN_ROLE)
-                .requestMatchers(HttpMethod.DELETE, "/v1/users/delete/{id}").hasRole(ADMIN_ROLE)
-                .requestMatchers(HttpMethod.PUT, "/v1/users/update/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/v1/users/change-password").permitAll()
+                .requestMatchers(HttpMethod.GET, "/v1/users").hasRole(ADMIN)
+                .requestMatchers(HttpMethod.GET, "/v1/users/find/{id}").hasRole(ADMIN)
+                .requestMatchers(HttpMethod.DELETE, "/v1/users/delete/{id}").hasRole(ADMIN)
+                .requestMatchers(HttpMethod.PUT, "/v1/users/update/{id}").hasAnyRole(ADMIN, USER)
+                .requestMatchers(HttpMethod.POST, "/v1/users/change-password").hasAnyRole(ADMIN, USER)
         );
     }
 }
