7afb265ec12bc4d3c8fa60c067a54a8f0eaf398c
Rafael Silva <ainbot6@gmail.com>
2024-05-09 16:39:16 -0300

    feat: add Swagger endpoints

--- src/main/java/org/instituteatri/deep/infrastructure/security/SecurityConfigurations.java
+++ src/main/java/org/instituteatri/deep/infrastructure/security/SecurityConfigurations.java
@@ -33,6 +33,29 @@ public class SecurityConfigurations {
         return web -> web.ignoring()
                 .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
     }
+
+    /**
+     * Configures the security filter chain for the application.
+     * <p>
+     * This method applies specific security configurations for authentication and user details,
+     * sets up CSRF protection with a custom cookie CSRF token repository, configures session management
+     * to be stateless, and defines authorization rules for various endpoints. It also configures a custom
+     * filter for processing security-related tasks before the standard username and password authentication
+     * filter, and sets up custom logout behavior.
+     * </p>
+     * <p>
+     * To add new routes in an organized way, follow the pattern used in the AuthSecurityConfig and UserSecurityConfig classes.
+     * Create separate classes to configure the authorization rules for each set of endpoints. In these classes, define specific
+     * methods to apply the corresponding security settings to the desired endpoints, similar to the methods
+     * used in AuthSecurityConfig and UserSecurityConfig. When you have finished configuring a new class, integrate it into the chain of
+     * application's security filters, without the need to use the applyAuthSecurityConfig or applyUserSecurityConfig methods,
+     * by creating an instance of the class directly in the configure(HttpSecurity http) method of the main security configuration class.
+     * </p>
+     *
+     * @param httpSecurity the {@link HttpSecurity} to configure
+     * @return the configured {@link SecurityFilterChain}
+     * @throws Exception if an error occurs during the configuration
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
@@ -42,7 +65,20 @@ public class SecurityConfigurations {
         return httpSecurity
                 .csrf(csrf -> csrf.csrfTokenRepository(customizeCookieCsrfTokenRepository()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/auth/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html"
+                        ).permitAll().anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout(logout -> logout.logoutUrl("/v1/auth/logout")
                         .logoutSuccessUrl("/v1/auth/login")
@@ -70,6 +106,7 @@ public class SecurityConfigurations {
     private void applyUserSecurityConfig(HttpSecurity http) throws Exception {
         userSecurityConfig.configure(http);
     }
+
     private CsrfTokenRepository customizeCookieCsrfTokenRepository() {
         CookieCsrfTokenRepository tokenRepository = new CookieCsrfTokenRepository();
         tokenRepository.setCookieCustomizer(builder ->
