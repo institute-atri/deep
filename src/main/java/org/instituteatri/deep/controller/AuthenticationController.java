602a97c2ab56596267f7ee91a3d375ce93930222
Rafael Silva <ainbot6@gmail.com>
2024-05-09 21:15:34 -0300

    feat(docs): add swagger documentation to endpoints (#6)

--- src/main/java/org/instituteatri/deep/controller/AuthenticationController.java
+++ src/main/java/org/instituteatri/deep/controller/AuthenticationController.java
@@ -1,5 +1,12 @@
 package org.instituteatri.deep.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -19,31 +26,138 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication Controller", description = "Endpoints for user authentication management")
 public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
     private final AccountService accountService;
 
+    @Operation(
+            method = "POST",
+            summary = "Authenticate user by verifying credentials.",
+            description = "Endpoint for authentication. " +
+                    "If a user enters the wrong password more than four times," +
+                    " their account will be locked. Upon successful login, " +
+                    "returns a token and refresh-token.")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{" +
+                                            "\"token\": [\"string\"]," +
+                                            "\"refresh-token\": [\"string\"]" +
+                                            "}"
+                            ))),
+
+            @ApiResponse(responseCode = "404", description = "Not found.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"Invalid username or password.\"}"
+                            ))),
+
+            @ApiResponse(responseCode = "403", description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"Account is locked.\"}"
+                            ))),
+    })
     @PostMapping("/login")
     public ResponseEntity<TokenResponseDTO> getLogin(@RequestBody LoginRequestDTO authDto) {
         return accountService.loginAccount(authDto, authenticationManager);
     }
 
+    @Operation(
+            method = "POST",
+            summary = "Register a new user.",
+            description = "Registers a new user in the system." +
+                    " The user information should be provided in the request body." +
+                    " Upon successful registration, the endpoint returns a token and a refresh token," +
+                    " which can be used for authentication and authorization in subsequent requests.")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{" +
+                                            "\"token\": [\"string\"]," +
+                                            "\"refresh-token\": [\"string\"]" +
+                                            "}"
+                            ))),
+
+            @ApiResponse(responseCode = "409", description = "Conflicts.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"E-mail not available.\"}"
+                            )))
+    })
     @PostMapping("/register")
     public ResponseEntity<TokenResponseDTO> register(@RequestBody @Valid RegisterRequestDTO registerRequestDTO) {
         return accountService.registerAccount(registerRequestDTO);
     }
 
+
+    @Operation(
+            method = "POST",
+            summary = "Logout the current user.",
+            description = "Endpoint to logout the currently authenticated user. " +
+                    "Invalidates the current session and clears the security context.")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success."),
+            @ApiResponse(responseCode = "401", description = "Unauthorized.")
+    })
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         new SecurityContextLogoutHandler().logout(request, response, authentication);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(
+            method = "POST",
+            summary = "Refresh the access token using a refresh token.",
+            description = "Endpoint to refresh the access token using a valid refresh token.")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{" +
+                                            "\"token\": [\"string\"]," +
+                                            "\"refresh-token\": [\"string\"]" +
+                                            "}"
+                            ))),
+
+            @ApiResponse(responseCode = "404", description = "Not found.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"User email not found in token.\"}"
+                            ))),
+
+            @ApiResponse(responseCode = "403", description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"User unauthorized.\"}"
+                            )))
+    })
     @PostMapping("/refresh-token")
     public ResponseEntity<TokenResponseDTO> refreshToken(@RequestBody RefreshTokenRequestDTO tokenDTO) {
         return accountService.refreshToken(tokenDTO);
     }
+
+    @Operation(
+            method = "GET",
+            summary = "Retrieve CSRF token.",
+            description = "Retrieves the Cross-Site Request Forgery (CSRF) token for the current session." +
+                    " The CSRF token is a security mechanism used to prevent" +
+                    " CSRF attacks by ensuring that requests originate from the same site.")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CsrfToken.class)
+                    ))
+    })
     @GetMapping("/csrf-token")
     public CsrfToken csrf(HttpServletRequest request) {
         return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
--- src/main/java/org/instituteatri/deep/controller/UserController.java
+++ src/main/java/org/instituteatri/deep/controller/UserController.java
@@ -1,5 +1,11 @@
 package org.instituteatri.deep.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.instituteatri.deep.dto.request.RegisterRequestDTO;
@@ -15,21 +21,117 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
+@Tag(name = "User Controller", description = "Endpoints for user management")
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(
+            method = "GET",
+            summary = "Get all users.",
+            description = "Returns a list of all users registered in the system, only with the admin token.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Success.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{" +
+                                            "\"id\":\"string\"" +
+                                            ",\"name\":\"string\"," +
+                                            "\"email\":\"string\"}"
+                            ))),
+
+            @ApiResponse(responseCode = "403", description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"User isn't authorized.\"}"
+                            )))
+    })
     @GetMapping()
     public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
         return userService.getAllUsers();
     }
 
+
+    @Operation(
+            method = "GET",
+            summary = "Get an user by ID",
+            description = "Returns the user with the specified ID. Only users with the ADMIN role can search by user.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Success.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{" +
+                                            "\"id\":\"string\"" +
+                                            ",\"name\":\"string\"," +
+                                            "\"email\":\"string\"}"
+                            ))),
+
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not found.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"Could not find user with id:661eff5e4af2c96e8a7dedc92\"}"
+                            ))),
+
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"User isn't authorized.\"}"
+                            )))
+    })
     @GetMapping("/find/{id}")
     public ResponseEntity<UserResponseDTO> getByUserId(@PathVariable String id) {
         UserResponseDTO user = userService.getByUserId(id);
         return ResponseEntity.ok().body(user);
     }
 
+    @Operation(
+            method = "PUT",
+            summary = "Update an event.",
+            description = "Updates the user identified by the specified ID. " +
+                    "Only authenticated users with a valid token are authorized to perform this operation.")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{" +
+                                            "\"token\": [\"string\"]," +
+                                            "\"refresh-token\": [\"string\"]" +
+                                            "}"
+                            ))),
+
+            @ApiResponse(responseCode = "401", description = "Unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"User isn't authenticated.\"}"
+                            ))),
+
+            @ApiResponse(responseCode = "403", description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"User isn't authorized.\"}"
+                            ))),
+
+            @ApiResponse(responseCode = "404", description = "Not found.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"Could not find user with id:661eff5e4af2c96e8a7dedc92\"}"
+                            ))),
+
+            @ApiResponse(responseCode = "409", description = "Conflicts.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"E-mail not available.\"}"
+                            )))
+    })
     @PutMapping("/update/{id}")
     public ResponseEntity<TokenResponseDTO> updateUser(
             @PathVariable String id,
@@ -39,6 +141,32 @@ public class UserController {
         return userService.updateUser(id, registerRequestDTO, authentication);
     }
 
+    @Operation(
+            method = "DELETE",
+            summary = "Delete an user by ID",
+            description = "Deletes the user with the specified ID. Only users with the ADMIN role can delete the user.")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No content."),
+
+            @ApiResponse(responseCode = "401", description = "Unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"User isn't authenticated.\"}"
+                            ))),
+
+            @ApiResponse(responseCode = "403", description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"User isn't authorized.\"}"
+                            ))),
+
+            @ApiResponse(responseCode = "404", description = "Not found.",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"message\":\"Could not find user with id:661eff5e4af2c96e8a7dedc92\"}"
+                            ))),
+    })
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable String id) {
         return userService.deleteUser(id);
