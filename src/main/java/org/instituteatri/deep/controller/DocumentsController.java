d39c59fbebdd3e2cd10511b9e54d31f198bc43ca
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-05-15 19:10:46 -0300

    feat: add response entity on controllers (#12)

--- src/main/java/org/instituteatri/deep/controller/DocumentsController.java
+++ src/main/java/org/instituteatri/deep/controller/DocumentsController.java
@@ -1,41 +1,56 @@
 package org.instituteatri.deep.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.instituteatri.deep.service.DocumentsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.instituteatri.deep.dto.response.DocumentsResponseDTO;
+import org.springframework.web.bind.annotation.*;
 
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/documents")
+@RequiredArgsConstructor
 public class DocumentsController {
+
+    private final DocumentsService documentsService;
+    private static final Logger logger = LoggerFactory.getLogger(DocumentsController.class);
+
     @PostMapping("/documents")
-    public String getDocuments() { return "Hello, Deep!"; }
+    ResponseEntity<DocumentsResponseDTO> createDocument(@RequestBody DocumentsResponseDTO request) {
+        DocumentsResponseDTO response = documentsService.createDocuments(request);
+        logger.info("Creating document with ID {}", request.getId());
+        return ResponseEntity.ok(documentsService.createDocuments(request));
+    }
 
     @GetMapping("/documents/{id}")
-    public String getDocumentsById() {
-        return "Hello, Deep!";
-    }
-
-    @GetMapping("/cases/{casesId}/documents")
-    public String getDocumentsByCasesId() {
-        return "Hello, Deep!";
-    }
-
-    @PutMapping("/documents/{id}")
-    public String updateDocumentsById() {
-        return "Hello, Deep!";
+    public ResponseEntity<DocumentsResponseDTO> getDocumentById(@PathVariable String id) {
+        logger.info("Getting document by ID {}", id);
+        return ResponseEntity.ok(documentsService.getDocumentsById(id));
     }
 
     @DeleteMapping("/documents/{id}")
-    public String deleteDocumentsById() {
-        return "Hello, Deep!";
+    public ResponseEntity<Void> deleteDocumentById(@PathVariable String id) {
+        documentsService.deleteDocumentById(id);
+        logger.info("Deleting document by ID {}", id);
+        return ResponseEntity.ok().build();
     }
 
+    @PutMapping("/documents/{id}")
+    public ResponseEntity<DocumentsResponseDTO> updateDocumentById(@RequestBody DocumentsResponseDTO request) {
+        documentsService.updateDocumentsById(request);
+        logger.info("Updating document by ID {}", request.getId());
+        return ResponseEntity.ok(documentsService.updateDocumentsById(request));
+    }
+
+    @GetMapping("/documents/all")
+    public ResponseEntity<List<DocumentsResponseDTO>> getAllDocuments() {
+        logger.info("Getting all documents");
+        return ResponseEntity.ok(documentsService.getAllDocuments());
+    }
 
 
 
--- src/main/java/org/instituteatri/deep/controller/EvidencesController.java
+++ src/main/java/org/instituteatri/deep/controller/EvidencesController.java
@@ -1,42 +1,61 @@
 package org.instituteatri.deep.controller;
 
 
+import lombok.RequiredArgsConstructor;
+import org.instituteatri.deep.service.EvidencesService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.instituteatri.deep.dto.response.EvidencesResponseDTO;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
 
 @RestController
 @RequestMapping("/evidences")
+@RequiredArgsConstructor
 public class EvidencesController {
+
+    private final EvidencesService evidencesService;
+    private static final Logger logger = LoggerFactory.getLogger(EvidencesController.class);
 
 
     @PostMapping("/evidences")
-    public String getEvidences() {return "Hello, Deep!";}
+
+public ResponseEntity<EvidencesResponseDTO> createEvidence(@RequestBody EvidencesResponseDTO request) {
+EvidencesResponseDTO response = evidencesService.createEvidences(request);
+        logger.info("Creating evidence with ID {}", request.getId());
+        return ResponseEntity.ok(evidencesService.createEvidences(request));
+    }
 
     @GetMapping("/evidences/{id}")
-    public String getEvidencesById() {
-        return "Hello, Deep!";
-    }
-
-    @GetMapping("/cases/{casesId}/evidences")
-    public String getEvidencesByCasesId() {
-        return "Hello, Deep!";
-    }
-
-    @PutMapping("/evidences/{id}")
-    public String updateEvidencesById() {
-        return "Hello, Deep!";
+    public ResponseEntity<EvidencesResponseDTO> getEvidenceById(@PathVariable String id) {
+        logger.info("Getting evidence by ID {}", id);
+        return ResponseEntity.ok(evidencesService.getEvidenceById(id));
     }
 
     @DeleteMapping("/evidences/{id}")
-    public String deleteEvidencesById() {
-        return "Hello, Deep!";
+    public ResponseEntity<Void> deleteEvidenceById(@PathVariable String id) {
+        evidencesService.deleteEvidenceById(id);
+        logger.info("Deleting evidence by ID {}", id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/evidences/{id}")
+    public ResponseEntity<EvidencesResponseDTO> updateEvidenceById(@RequestBody EvidencesResponseDTO request) {
+        evidencesService.updateEvidencesById(request);
+        logger.info("Updating evidence by ID {}", request.getId());
+        return ResponseEntity.ok(evidencesService.updateEvidencesById(request));
+    }
+
+    @GetMapping("/evidences/all")
+    public ResponseEntity<List<EvidencesResponseDTO>> getAllEvidences() {
+        logger.info("Getting all evidences");
+        return ResponseEntity.ok(evidencesService.getAllEvidences());
     }
 
 
+
 }
+
