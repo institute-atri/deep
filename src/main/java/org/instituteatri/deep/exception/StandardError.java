cf42f65bdefb4bc36d4b0da8a8f0587fc2086c92
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-05-15 16:42:23 -0300

    feat: add standard error (#12)

--- src/main/java/org/instituteatri/deep/exception/StandardError.java
+++ src/main/java/org/instituteatri/deep/exception/StandardError.java
@@ -1,4 +1,16 @@
 package org.instituteatri.deep.exception;
 
-public class StarndarError {
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class StandardError {
+    private String message;
+    private Integer status;
+    private Long timestamp;
+    private String path;
+    private String error;
 }
+
+
