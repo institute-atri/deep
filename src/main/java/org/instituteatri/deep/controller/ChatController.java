2b374f8cd160e1b90f0b4e434ba031e2ea781345
Kevin Garcia <kevingarciarp@gmail.com>
2024-06-11 21:32:15 -0300

    feat(docs): Add documentation for generating chat response using Gemini

--- src/main/java/org/instituteatri/deep/controller/ChatController.java
+++ src/main/java/org/instituteatri/deep/controller/ChatController.java
@@ -3,6 +3,7 @@ package org.instituteatri.deep.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.instituteatri.deep.dto.request.ChatRequestDTO;
+import org.instituteatri.deep.dto.response.ChatResponseDTO;
 import org.instituteatri.deep.service.ChatService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,9 +31,25 @@ public class ChatController {
             }
     )
     @PostMapping("/ai/generate/{occurrenceId}")
-    public ResponseEntity<OllamaApi.ChatResponse> generate(@RequestBody ChatRequestDTO request,
-            @PathVariable String occurrenceId) {
-        LOGGER.info("Received request to generate a chat response");
-        return ResponseEntity.ok(service.generate(request, occurrenceId));
+    public ResponseEntity<OllamaApi.ChatResponse> generateMessageFromOllama(@RequestBody ChatRequestDTO request,
+                                                                            @PathVariable String occurrenceId) {
+        LOGGER.info("Received request to generate a chat-ollama response");
+        return ResponseEntity.ok(service.generateMessageResponseFromOllama(request, occurrenceId));
+    }
+
+    @Operation(
+            method = "POST",
+            summary = "Generate a chat response for the occurrence using Gemini.",
+            description = "Endpoint to generate a specific chat response for the occurrence identified by the 'occurrenceId' parameter. " +
+                    "It processes chat data to produce an appropriate response and returns the generated chat response.",
+            responses ={
+                    @ApiResponse(responseCode = "200", description = "Success."),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized.")
+            }
+    )
+    @PostMapping("/ai-gemini/generate/{occurrenceId}")
+    public ResponseEntity<ChatResponseDTO> generateMessageFromGemini(@RequestBody ChatRequestDTO request, @PathVariable String occurrenceId) {
+        LOGGER.info("Received request to generate a chat-gemini response");
+        return ResponseEntity.ok(service.generateMessageResponseFromGemini(request, occurrenceId));
     }
 }
